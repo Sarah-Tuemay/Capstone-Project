235791a565f758b98e694f3decfa14588cf992a2
Sarah <184196481+Sarah-Tuemay@users.noreply.github.com>
2025-04-14 16:05:02 +0300

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,78 +1,174 @@
+
+// 🔹 DOM Elements
 const movieTitle = document.querySelector(".movieTitle");
-const searchingButton = document.querySelector(".searchingButton");
+
 const bigContainer = document.querySelector(".bigConatiner");
 const container = document.querySelector(".container");
+const heading = document.querySelector(".heading");
 
 
-searchingButton.addEventListener("click", function(){
-    fetchDAta();
-});
 
+    const searchingButton = document.querySelector(".searchingButton");
+    const trendingButton = document.querySelector(".trending");
+    const home = document.querySelector(".home");
+    const nowPlayingButton = document.querySelector(".nowPlaying");
+  
+    home.addEventListener("click", function() {
+        fetchTopRated();
+    });
+    
+    nowPlayingButton.addEventListener("click", function() {
+        nowPlaying();
+    });
+    trendingButton.addEventListener("click", function() {
+       fetchTrending();
+    });
+    
+    
+    // 🔹 Search Button Click
+    searchingButton.addEventListener("click", function () {
+        fetchDataBySearching();
+    });
+
+
+
+// 🔹 Pressing Enter Key in Search Field
 movieTitle.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-        fetchDAta();
+        fetchDataBySearching();
     }
 });
 
-async function fetchDAta() {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=d41e0d74fd90ed16c99aae12cd0c1976&query=${movieTitle.value}`; 
+function fetchTopRated() {
+    heading.innerText = "Top rated Movies";
+    let url = `https://api.themoviedb.org/3/movie/top_rated?api_key=d41e0d74fd90ed16c99aae12cd0c1976`;
+    fetchData(url);
+}
+
+function fetchDataBySearching() {
+    heading.innerText = `${movieTitle.value}`;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=d41e0d74fd90ed16c99aae12cd0c1976&query=${movieTitle.value}`;
+    fetchData(url);
+}
+
+function fetchTrending () {
+    heading.innerText = "Trending Movies";
+    let url = 'https://api.themoviedb.org/3/trending/movie/day?api_key=d41e0d74fd90ed16c99aae12cd0c1976';
+    fetchData(url); 
+
+}
+
+function nowPlaying() {
+    heading.innerText = "Now Playing";
+    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=d41e0d74fd90ed16c99aae12cd0c1976`;
+    fetchData(url);
+}
+
+
+async function fetchData(url) {
+
+    // let url = `https://api.themoviedb.org/3/search/movie?api_key=d41e0d74fd90ed16c99aae12cd0c1976&query=${movieTitle.value}`;
+
     try {
         const response = await fetch(url);
         const data = await response.json();
-        let length = Object.keys(data).length; // to know how many movies are under the same title
+        const length = data.results.length;
 
-        let newContainer = document.createElement("div");
-        newContainer.classList.add("container");
+        if(length == 0) {
+            heading.innerText = "";
+            console.log(length,err);
+            bigContainer.innerHTML = "";
+
+            const errorDiv = document.createElement("div");
+            const errorEmoji = document.createElement("img");
+            const errorContent = document.createElement("h1");
         
-        for(let i = 0; i< length ; i++) {
+            errorDiv.classList.add("errorDiv");
+            errorEmoji.src = "assets\\Teary_Eyed-removebg-preview.png";
+            errorEmoji.classList.add("errorEmoji");
+            errorContent.classList.add("errorContent");
+        
+            errorContent.innerText = `O...O Sorry mate I think you have entered the wrong title. Try again!!`;
+        
+            errorDiv.appendChild(errorEmoji);
+            errorDiv.appendChild(errorContent);
+            bigContainer.appendChild(errorDiv);
+        } else {
+            console.log(length, data);
+        const newContainer = document.createElement("div");
+        newContainer.classList.add("container");
 
-         //division for the detail about the movie("poster, title, release date and over view ")
-            let movieDivision = document.createElement("div");
+        for (let i = 0; i < length; i++) {
+            const movieDivision = document.createElement("div");
+            const movieDetails = document.createElement("div");
 
-            movieDivision.classList.add("content"); 
-            let imgPoster  = data.results[i].backdrop_path;
+            movieDivision.classList.add("content");
+
+            const imgPoster = data.results[i].poster_path;
             const img = document.createElement("img");
             img.src = `https://image.tmdb.org/t/p/w780${imgPoster}`;
             img.alt = "A poster of a movie";
-    
+
             const title = document.createElement("p");
             const overview = document.createElement("p");
             const releaseDate = document.createElement("p");
 
-            title.innerHTML = `<b>Title: </b> ${data.results[i].original_title}`;
+            title.innerHTML = `${data.results[i].original_title}`;
             overview.innerHTML = `<b>Over View: </b> ${data.results[i].overview}`;
             releaseDate.innerHTML = `<b>Release Date: </b> ${data.results[i].release_date}`;
 
+            movieDetails.style.display = "none";
+
+            // 🔸 Show details on image click
+            img.addEventListener("click", function () {
+                movieDetails.style.display = "block";
+            });
+
+            movieDetails.appendChild(releaseDate);
+            movieDetails.appendChild(overview);
+
             movieDivision.appendChild(img);
             movieDivision.appendChild(title);
-            movieDivision.appendChild(overview);
-            movieDivision.appendChild(releaseDate);
+            movieDivision.appendChild(movieDetails);
             newContainer.appendChild(movieDivision);
         }
-        bigContainer.innerHTML = ""; // clearing the previous contents in the page
-        bigContainer.appendChild(newContainer);
-    }
-    catch(err) {
+
+        // 🔸 Clear old content and show new
         bigContainer.innerHTML = "";
-
-        let errorDiv = document.createElement("div"); 
-        let errorEmoji = document.createElement("img");
-        let errorContent = document.createElement("h1"); 
-
-        errorDiv.classList.add("errorDiv");
-        errorEmoji.src = "assets\\sad sticker for error.gif";
-        errorEmoji.classList.add("errorEmoji");    
-        errorContent.innerText = `O...O Sorry mate i think you have entered the wrong title try again!!`;
-        errorContent.classList.add("errorContent");
-
-        errorDiv.appendChild(errorEmoji);
-        errorDiv.appendChild(errorContent);
-        bigContainer.appendChild(errorDiv);
+        bigContainer.appendChild(newContainer);
+        }
         
-        // the above code creates 
-        // <div>
-        //     <img> 
-        //      <h1> </h1>
-        // </div>
+        
+    }
+    catch (err) {
+       
+            console.log(length,err);
+            bigContainer.innerHTML = "";
+
+            const errorDiv = document.createElement("div");
+            const errorEmoji = document.createElement("img");
+            const errorContent = document.createElement("h1");
+        
+            errorDiv.classList.add("errorDiv");
+            errorEmoji.src = "assets\\Teary_Eyed-removebg-preview.png";
+            errorEmoji.classList.add("errorEmoji");
+            errorContent.classList.add("errorContent");
+        
+            errorContent.innerText = `O...O Sorry mate I think you have entered the wrong title. Try again!!`;
+        
+            errorDiv.appendChild(errorEmoji);
+            errorDiv.appendChild(errorContent);
+            bigContainer.appendChild(errorDiv);
+        
     }
 }
+
+// 🔹 Error Message UI
+function displayError() {
+//
+}
+
+fetchTopRated();
+
+
+
